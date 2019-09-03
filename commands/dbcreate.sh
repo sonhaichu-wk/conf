250aabcc3c5fb24ed537091973f22334569cf799
sonhaichu <sonhaichu@gmail.com>
2019-09-03 11:29:34 +0700

    update dbcreate command

--- commands/dbcreate.sh
+++ commands/dbcreate.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-mysql --login-path=client -e "create database if not exists $1"
+char='utf8mb4'
+collate='utf8mb4_general_ci'
+
+if [ -n "$2" ]; then
+	char=$2
+fi
+
+if [ -n "$3" ]; then
+	collate=$3
+fi
+
+mysql --login-path=client -e "create database if not exists $1 character set $char collate $collate"
