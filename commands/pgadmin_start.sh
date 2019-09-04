cd3f330faeef69f1537ce0e1ceb51b07dd4dcdca
sonhaichu <sonhaichu@gmail.com>
2019-09-04 11:39:54 +0700

    update

--- commands/pgadmin_start.sh
+++ commands/pgadmin_start.sh
@@ -2,6 +2,7 @@
 
 docker pull dpage/pgadmin4 &&
 docker run -p 5050:80 \
+		-v "/var/lib/pgadmin:/var/lib/pgadmin" \
     -e "PGADMIN_DEFAULT_EMAIL=sonhaichu@gmail.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=secret" \
     -d dpage/pgadmin4
