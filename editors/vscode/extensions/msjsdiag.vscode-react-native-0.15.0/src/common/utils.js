541837a9eb2e45903546f037b01e364399fc4956
sonhaichu <sonhaichu@gmail.com>
2020-05-14 12:02:15 +0700

    update

---
+++ editors/vscode/extensions/msjsdiag.vscode-react-native-0.15.0/src/common/utils.js
@@ -0,0 +1,11 @@
+"use strict";
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+function getFileNameWithoutExtension(fileName) {
+    return path.basename(fileName, path.extname(fileName));
+}
+exports.getFileNameWithoutExtension = getFileNameWithoutExtension;
+
+//# sourceMappingURL=utils.js.map
